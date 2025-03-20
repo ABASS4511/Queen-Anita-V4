c0d73a3a1df1b9f3e16e04626752f3b77b70727e
ABASS4511 <sawadogoabdoul611@gmail.com>
2025-03-20 05:59:25 +0000

    Update config.js

--- config.js
+++ config.js
@@ -3,15 +3,15 @@ require('dotenv').config();
 global.owner = process.env.OWNER_NUMBER.split(','); 
 global.anitav4 = process.env.BOT_NAME || "𝐐𝐔𝐄𝐄𝐍_𝐀𝐍𝐈𝐓𝐀-𝐕𝟒";
 global.ownername = process.env.OWNER_NAME || "David Cyril";
-global.packname = process.env.PACK_NAME || "𝐐𝐔𝐄𝐄𝐍_𝐀𝐍𝐈𝐓𝐀-𝐕𝟒";
-global.author = process.env.AUTHOR || "𝐃𝐀𝐕𝐈𝐃 𝐂𝐘𝐑𝐈𝐋";
+global.packname = process.env.PACK_NAME || "༺𝙏𝙈𝙋 ᵈᵃʳᵏ ˡᵘᶠᶠʸ ༻𓅓";
+global.author = process.env.AUTHOR || "༺𝙏𝙈𝙋 ᵈᵃʳᵏ ˡᵘᶠᶠʸ ༻𓅓";
 global.channelname = process.env.CHANNEL_NAME || "𝐃𝐀𝐕𝐈𝐃 𝐂𝐘𝐑𝐈𝐋";
 global.channeljid = process.env.CHANNEL_JID || "120363315231436175@newsletter";
 global.autoTyping = process.env.AUTO_TYPING === 'true'; 
 global.autoRecord = process.env.AUTO_RECORD === 'true';
 global.autoViewStatus = process.env.AUTO_VIEW_STATUS === 'true';
 global.AUTOSTATUS_REACT = process.env.AUTOSTATUS_REACT === 'true';
-global.autoLikeEmoji = process.env.AUTO_LIKE_EMOJI || "💚";
+global.autoLikeEmoji = process.env.AUTO_LIKE_EMOJI || "💚,👻,🤭,😭,😶‍🌫️,🫥,😍,🥲,🤑";
 
 global.LEVELUP = process.env.LEVELUP === 'true';
 
@@ -30,7 +30,7 @@ global.roles = {
 
 global.sudoUsers = process.env.SUDO_USERS.split(',').map(user => `${user}@s.whatsapp.net`); 
 
-global.public = process.env.PUBLIC === 'true';
+global.public = process.env.PUBLIC === 'false';
 
 global.ANTIDELETE = process.env.ANTIDELETE === 'true';
 global.unavailable = process.env.UNAVAILABLE === 'true';
@@ -41,7 +41,7 @@ global.autoreact = process.env.AUTO_REACT === 'true';
 
 global.WELCOME = process.env.WELCOME === 'true';
 
-global.prefix = process.env.PREFIX || '.';
+global.prefix = process.env.PREFIX || '&';
 
 global.autobio = process.env.AUTO_BIO === 'true';
 
